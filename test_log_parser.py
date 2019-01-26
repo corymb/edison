220e1c0015928d6beafe05a48b7c2d12e5361968
Dave Bush <github@david-bush.co.uk>
2019-01-26 15:46:18 +1300

    Added Python deps and skeleton tests.

---
+++ test_log_parser.py
@@ -0,0 +1,18 @@
+def test_get_time():
+    pass
+
+
+def test_get_date():
+    pass
+
+
+def test_get_nick():
+    pass
+
+
+def test_get_recipient():
+    pass
+
+
+def test_get_message():
+    pass
