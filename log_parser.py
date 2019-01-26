2d173eb3a7ffcb5b0e25c3f825a0a081501d9cbc
Dave Bush <github@david-bush.co.uk>
2019-01-26 16:32:42 +1300

    Added message parser and moved functions to a class.

--- log_parser.py
+++ log_parser.py
@@ -1,36 +1,48 @@
 from datetime import datetime
 
 
-def get_date(date_string):
+class LogParser:
     """
-    Takes date in YYYY-MM-DD form,
-    returns datetime object
+    Groups log parsing functionality.
+    Methods not prefixed with '_' should be considered a public interface
     """
-    return datetime.strptime(date_string, '%Y-%m-%d')
+    def _get_date(self, date_string):
+        """
+        Takes date in YYYY-MM-DD form,
+        returns datetime object
+        """
+        return datetime.strptime(date_string, '%Y-%m-%d')
 
+    def _get_time(self, time_string):
+        """
+        Takes time in [HH:MM:SS] form,
+        returns time object
+        """
+        return datetime.strptime(time_string, '[%H:%M:%S]').time()
 
-def get_time(time_string):
-    """
-    Takes time in [HH:MM:SS] form,
-    returns time object
-    """
-    return datetime.strptime(time_string, '[%H:%M:%S]').time()
+    def _get_nick(self, line):
+        """
+        Takes a list based on the line to parse
+        returns user's nick
+        """
+        sender = line[0]
+        # strip < and >:
+        return sender[1:-1]
 
+    def _get_recipient(self, line):
+        """
+        Takes a list based on the line to parse
+        returns recipient's nick if it exists, None if not
+        """
+        recipient = line[1]
+        return recipient[:-1] if recipient.endswith(':') else None
 
-def get_nick(line):
-    """
-    Takes a list based on the line to parse
-    returns user's nick
-    """
-    sender = line[0]
-    # strip < and >:
-    return sender[1:-1]
-
-
-def get_recipient(line):
-    """
-    Takes a list based on the line to parse
-    returns recipient's nick if it exists, None if not
-    """
-    recipient = line[1]
-    return recipient[:-1] if recipient.endswith(':') else None
+    def _get_message(self, line, recipient=None):
+        """
+        Takes a list based on the line to parse
+        returns message string excluding recipient if not None
+        """
+        # Strip recipient nick from message if it's in there:
+        if recipient and recipient + ':' in line:
+            line = line[2:]
+        return ' '.join(line)
--- test_log_parser.py
+++ test_log_parser.py
@@ -1,36 +1,45 @@
 from datetime import datetime, time
 
-from log_parser import get_date, get_time, get_nick, get_recipient
+from log_parser import LogParser
 
 
 LINE = '<cdunklau> JordiGH: angle brackets'.split()
 
 
 def test_get_date():
+    log_parser = LogParser()
     date = '2019-01-01'
     expected = datetime(year=2019, month=1, day=1)
-    assert get_date(date) == expected
+    assert log_parser._get_date(date) == expected
 
 
 def test_get_time():
+    log_parser = LogParser()
     time_string = '[03:43:05]'
     expected = time(hour=3, minute=43, second=5)
-    assert get_time(time_string) == expected
+    assert log_parser._get_time(time_string) == expected
 
 
 def test_get_nick():
+    log_parser = LogParser()
     expected = 'cdunklau'
-    assert get_nick(LINE) == expected
+    assert log_parser._get_nick(LINE) == expected
 
 
 def test_get_recipient():
+    log_parser = LogParser()
     expected = 'JordiGH'
-    assert get_recipient(LINE) == expected
+    assert log_parser._get_recipient(LINE) == expected
+
 
 def test_get_recipient_without_recipientp():
+    log_parser = LogParser()
     # strips recipient:
     line = [LINE[0]] + LINE[2:]
-    assert get_recipient(line) == None
+    assert log_parser._get_recipient(line) is None
+
 
 def test_get_message():
-    pass
+    log_parser = LogParser()
+    expected = ' '.join(LINE[2:])
+    assert log_parser._get_message(LINE, recipient='JordiGH') == expected
