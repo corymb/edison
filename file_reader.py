0fe5beb042deab50089231acbba7bb01e6788948
Dave Bush <github@david-bush.co.uk>
2019-01-28 12:23:23 +1300

    Removed old versions of files.

--- file_reader.py
+++
@@ -1,7 +0,0 @@
-class FileReader:
-    def __init__(self, filename):
-        self.filename = filename
-
-    def read_file(self):
-        with open(self.filename) as f:
-            return f.readlines()
