1493b324abd84572da2cfae8e333b7117e365f0f
Dave Bush <github@david-bush.co.uk>
2019-01-28 14:24:38 +1300

    Added indexes to Line model.

--- search/models.py
+++ search/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-
 class Line(models.Model):
     SEARCHABLE_FIELDS = ('nick', 'recipient', 'message')
 
@@ -9,6 +8,13 @@ class Line(models.Model):
     nick = models.CharField(max_length=30)
     recipient = models.CharField(null=True, max_length=30)
     message = models.CharField(max_length=512)  # limit set in rfc 2812
+    # I would actually use a GIN Index for the message field, but this is fine
+    # for the volume of data we have.
 
     def __str__(self):
         return '{}'.format(self.message)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['nick', 'recipient', 'message']),
+        ]
